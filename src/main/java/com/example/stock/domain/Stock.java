e8d5b748bb255a906433acc36000694ec9a54bac
xanqus <sbk98031816@gmail.com>
2022-10-28 00:07:42 +0900

    create concurrency problem example

---
+++ src/main/java/com/example/stock/domain/Stock.java
@@ -0,0 +1,39 @@
+package com.example.stock.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Stock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long productId;
+
+    private Long quantity;
+
+    public Stock() {
+
+    }
+
+    public Stock(Long productId, Long quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+
+    public void decrease(Long quantity) {
+        if(this.quantity - quantity < 0) {
+            throw new RuntimeException("stock이 0 이하입니다.");
+        }
+
+        this.quantity -= quantity;
+    }
+}
---
+++ src/test/java/com/example/stock/StockApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.stock;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
