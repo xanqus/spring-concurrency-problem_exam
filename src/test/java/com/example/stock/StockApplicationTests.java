7482f0073955ae628d56512e68ea0b59006b2be3
xanqus <sbk98031816@gmail.com>
2022-10-28 13:45:56 +0900

    create concurrency problem example test

--- src/test/java/com/example/stock/StockApplicationTests.java
+++ src/test/java/com/example/stock/StockApplicationTests.java
@@ -1,15 +1,64 @@
 package com.example.stock;
 
 
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import com.example.stock.service.StockService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class StockApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@BeforeEach
+	public void before() {
+		Stock stock = new Stock(1L, 100L);
 
+		stockRepository.saveAndFlush(stock);
+	}
+
+	@AfterEach
+	public void after() {
+		stockRepository.deleteAll();
+	}
+
+	@Autowired
+	private StockService stockService;
+
+	@Autowired
+	private StockRepository stockRepository;
+
+	@Test
+	void contextLoads() throws InterruptedException {
+
+		int threadCount = 100;
+		ExecutorService executorService = Executors.newFixedThreadPool(32);
+		CountDownLatch latch = new CountDownLatch(threadCount);
+
+		for(int i = 0; i < threadCount; i++) {
+			executorService.submit(() -> {
+				try {
+					stockService.decrease(1L, 1L);
+				} finally {
+					latch.countDown();
+				}
+			});
+		}
+		latch.await();
+
+		Stock stock = stockRepository.findById(1L).orElseThrow();
+
+		assertThat(stock.getQuantity())
+				.isEqualTo(0L);
 	}
 
 }
